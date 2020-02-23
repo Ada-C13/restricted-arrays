7df00375ad0885730612f6002f3f7d9d5c9695c5
Corinna Fabre <corinna.fabre@gmail.com>
2020-02-23 12:33:07 -0800

    Updated files with passing methods and time/space complexity explanations.

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -6,56 +6,119 @@ require_relative 'restricted_array.rb'
 
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we will need to loop through the whole array each time
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def length(array)
-  raise NotImplementedError
+  my_integer_array_length = 0
+  while array[my_integer_array_length] != nil
+    my_integer_array_length += 1
+  end
+  return my_integer_array_length
 end
 
 # Prints each integer values in the array
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we will need to loop through the whole array each time
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def print_array(array)
-  raise NotImplementedError
+  index = 0
+  while array[index] != nil
+    p array[index]
+    index += 1
+  end
 end
 
 # For an unsorted array, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because, worst case scenerio, we need to loop through the whole array each time
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def search(array, length, value_to_find)
-  raise NotImplementedError
+  index = 0
+  until index == length
+    if array[index] == value_to_find
+      return true
+      exit
+    else index += 1
+    end
+  end
+  
+  return false
 end
 
 # Finds and returns the largest integer value the array
 # Assumes that the array is not sorted.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we need to loop through the whole array each time because all numbers need to be checked
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def find_largest(array, length)
-  raise NotImplementedError
+  largest_number = array[0]
+  if largest_number
+    length.times do |index|
+      if array[index] > largest_number
+        largest_number = array[index]
+      end
+    end
+    return largest_number
+  end
 end
 
 # Finds and returns the smallest integer value in the array
 # Assumes that the array is not sorted.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we need to loop through the whole array each time because all numbers need to be checked
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def find_smallest(array, length)
-  raise NotImplementedError
+  smallest_number = array[0]
+  if smallest_number
+    length.times do |index|
+      if array[index] < smallest_number
+        smallest_number = array[index]
+      end
+    end
+    return smallest_number
+  end
 end
 
 # Reverses the values in the integer array in place
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) because we need to loop through the whole array each time because all numbers need to be checked
+# Space complexity: O(1) because no additional memory is required because no copy is made, the array is reversed in place
 def reverse(array, length)
-  raise NotImplementedError
+
+  index = 0
+  length = length - 1
+
+  while index < length
+    holding = array[index]
+    array[index] = array[length]
+    array[length] = holding
+
+    index += 1
+    length -= 1
+  end
+
+  return array
 end
 
 # For an array sorted in ascending order, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: best case efficiency of O(1) and worst case (average case) efficiency of O(log n)
+# Space complexity: O(1) because no additional memory is required because no copy is made
 def binary_search(array, length, value_to_find)
-  raise NotImplementedError
+  min_point = array[0]
+  max_point = length - 1
+  mid_point = length/2
+  
+  while min_point <= max_point
+    mid_point = (max_point + min_point) / 2
+
+    if array[mid_point] == value_to_find
+      return true
+    elsif mid_point < value_to_find
+      min_point = mid_point + 1
+    elsif mid_point > value_to_find
+      max_point = mid_point - 1
+    end
+  end
+
+  return false
+
 end
 
 # Helper method provided to sort the array in ascending order
