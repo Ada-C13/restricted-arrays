95e64249c5344fcdccfce67fc48890a9797d2f54
Yieni <yaynee8d@gmail.com>
2020-02-24 00:53:09 -0800

    final code with 4 failures

--- lib/using_restricted_array.rb
+++ lib/using_restricted_array.rb
@@ -6,58 +6,114 @@ require_relative 'restricted_array.rb'
 
 # Calculates the length of the restricted array. All values are integers.
 # The restricted_array is terminated by 'nil' i.e. array[length] = nil
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n). N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(1)  The space needed to store the count doesn't depend on the array size,
+#as the count value gets overridden until we reach the final count
 def length(array)
-  raise NotImplementedError
+  count = 0
+  x = 0
+  until array[x] == nil
+    count += 1
+    x += 1
+  end
+  return count
 end
 
 # Prints each integer values in the array
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(n) N reperesents each output from the array. 
+#The space needed for storage depends on how many elements are outputed from the array.
 def print_array(array)
-  raise NotImplementedError
+  x = 0
+  until array[x] == nil
+    puts array[x]
+  end
 end
 
 # For an unsorted array, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(1) The program will return true or fall no matter the size of the array.
 def search(array, length, value_to_find)
-  raise NotImplementedError
+  x = 0
+  until array[x] == nil
+    return true if array[x] == value_to_find
+    x += 1
+  end
+  return false
 end
 
 # Finds and returns the largest integer value the array
 # Assumes that the array is not sorted.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(1). The program will only return the 
+#largest value no matter the size of the array
 def find_largest(array, length)
-  raise NotImplementedError
+  largest = array[0]
+  x = 0
+  until array[x] == nil
+    largest = array[x] if array[x] > largest
+    x += 1
+  end
+  return largest
+  
 end
 
 # Finds and returns the smallest integer value in the array
 # Assumes that the array is not sorted.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)
+# Space complexity: O(1). The program will only return the 
+#smallest value no matter the size of the array
 def find_smallest(array, length)
-  raise NotImplementedError
+  smallest = array[0]
+  x = 0
+  until array[x] == nil
+    smallest = array[x] if array[x] < smallest
+    x += 1
+  end
+  return smallest
 end
 
 # Reverses the values in the integer array in place
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(n) The space needed for the reverse array depends on the size of the original array.
 def reverse(array, length)
-  raise NotImplementedError
+  reverse_array = []
+  x = 0
+  until array[x] == nil
+    reverse_array << array[length-(x+1)]
+    x += 1
+  end
+  return reverse_array
 end
 
 # For an array sorted in ascending order, searches for 'value_to_find'.
 # Returns true if found, false otherwise.
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n)N represents the number of elements in the array.
+    # The run time will depend on how many elements are in the array
+# Space complexity: O(1) The program will return true or fall no matter the size of the array.
 def binary_search(array, length, value_to_find)
-  raise NotImplementedError
-end
+  last = length - 1
+  middle = (last / 2)
+  length/2.times do
+    if array[0] == value_to_find || array[last] == value_to_find
+      return true
+    elsif array[middle] == value_to_find
+      return true
+    elsif array[middle] < value_to_find
+      middle = (middle + last)/2
+    elsif array[middle] > value_to_find
+      middle = (middle / 2)
+    end
+  end
+  return false
 
+end
 # Helper method provided to sort the array in ascending order
 # Implements selection sort
 # Time complexity = O(n^2), where n is the number of elements in the array.
